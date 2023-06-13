ab4f831de00245d2d53f8f8b0c9d5a417a15329c
Jonathan Christian <christianj707@gmail.com>
2023-06-13 11:55:12 -0700

    Initialize repository

---
+++ lib/jchristian27_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jchristian27_palindrome/version"
+
+module Jchristian27Palindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "jchristian27_palindrome"
+
+require "minitest/autorun"
